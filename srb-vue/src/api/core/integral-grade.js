c44c834e08949083229e2d7de9937580b3f82aac
yangchao <909833655@qq.com>
2022-10-12 23:42:25 +0800

    146

--- srb-vue/src/api/core/integral-grade.js
+++ srb-vue/src/api/core/integral-grade.js
@@ -12,4 +12,34 @@ export default {
       method: 'get',
     })
   },
+
+  updateById(integralGrade) {
+    return request({
+      url: '/admin/core/integralGrade/update',
+      method: 'put',
+      data: integralGrade,
+    })
+  },
+
+  removeById(id) {
+    return request({
+      url: '/admin/core/integralGrade/remove/' + id,
+      method: 'delete',
+    })
+  },
+
+  save(integralGrade) {
+    return request({
+      url: '/admin/core/integralGrade/save',
+      method: 'post',
+      data: integralGrade,
+    })
+  },
+
+  getById(id) {
+    return request({
+      url: `/admin/core/integralGrade/get/${id}`,
+      method: 'get',
+    })
+  },
 }

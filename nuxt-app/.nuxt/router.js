9bc3418c90bbb69d220263399f0c79c3db8de7ce
yangchao <909833655@qq.com>
2023-05-30 19:33:51 +0800

    219

--- nuxt-app/.nuxt/router.js
+++ nuxt-app/.nuxt/router.js
@@ -4,7 +4,13 @@ import { normalizeURL } from '@nuxt/ufo'
 import { interopDefault } from './utils'
 import scrollBehavior from './router.scrollBehavior.js'
 
-const _499571bb = () => interopDefault(import('..\\node_modules\\@nuxt\\vue-app\\template\\pages\\index.vue' /* webpackChunkName: "" */))
+const _b0b5688a = () => interopDefault(import('..\\pages\\about.vue' /* webpackChunkName: "pages/about" */))
+const _612a339e = () => interopDefault(import('..\\pages\\lend\\index.vue' /* webpackChunkName: "pages/lend/index" */))
+const _51fe3be6 = () => interopDefault(import('..\\pages\\user.vue' /* webpackChunkName: "pages/user" */))
+const _c5b3766e = () => interopDefault(import('..\\pages\\user\\user1.vue' /* webpackChunkName: "pages/user/user1" */))
+const _c597476c = () => interopDefault(import('..\\pages\\user\\user2.vue' /* webpackChunkName: "pages/user/user2" */))
+const _1fa27e4e = () => interopDefault(import('..\\pages\\lend\\_id.vue' /* webpackChunkName: "pages/lend/_id" */))
+const _7145b300 = () => interopDefault(import('..\\pages\\index.vue' /* webpackChunkName: "pages/index" */))
 
 // TODO: remove in Nuxt 3
 const emptyFn = () => {}
@@ -23,8 +29,34 @@ export const routerOptions = {
   scrollBehavior,
 
   routes: [{
-    path: "",
-    component: _499571bb
+    path: "/about",
+    component: _b0b5688a,
+    name: "about"
+  }, {
+    path: "/lend",
+    component: _612a339e,
+    name: "lend"
+  }, {
+    path: "/user",
+    component: _51fe3be6,
+    name: "user",
+    children: [{
+      path: "user1",
+      component: _c5b3766e,
+      name: "user-user1"
+    }, {
+      path: "user2",
+      component: _c597476c,
+      name: "user-user2"
+    }]
+  }, {
+    path: "/lend/:id",
+    component: _1fa27e4e,
+    name: "lend-id"
+  }, {
+    path: "/",
+    component: _7145b300,
+    name: "index"
   }],
 
   fallback: false

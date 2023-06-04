f2619aef225536f59d1f6e18559f485089b217ec
yangchao <909833655@qq.com>
2023-06-04 17:32:28 +0800

    251

--- srb-admin/src/router/index.js
+++ srb-admin/src/router/index.js
@@ -34,27 +34,27 @@ export const constantRoutes = [
   {
     path: '/login',
     component: () => import('@/views/login/index'),
-    hidden: true,
+    hidden: true
   },
 
   {
     path: '/404',
     component: () => import('@/views/404'),
-    hidden: true,
+    hidden: true
   },
 
   {
     path: '/',
-    component: Layout,
+    component: Layout, //布局组件
     redirect: '/dashboard',
     children: [
       {
         path: 'dashboard',
         name: 'Dashboard',
         component: () => import('@/views/dashboard/index'),
-        meta: { title: '首页', icon: 'dashboard' },
-      },
-    ],
+        meta: { title: '首页', icon: 'dashboard' }
+      }
+    ]
   },
 
   {
@@ -63,31 +63,30 @@ export const constantRoutes = [
     redirect: '/core/integral-grade/list',
     name: 'coreIntegralGrade',
     meta: { title: '积分等级管理', icon: 'el-icon-s-marketing' },
+    // false（默认值）：当且仅当父节点下只有一个子节点时，不显示父节点
+    // true：任何时候都显示父节点和子节点
     alwaysShow: true,
-    //false（默认）当且仅当父节点下只有一个子节点时，不显示父节点
-    //true，任何时候都显示父节点和子节点
-
     children: [
       {
         path: 'list',
-        name: 'coreIntegralGradeList', //每个路由节点的name名字不能相同
-        component: () => import('@/views/core/integral-grade/list'), //指向template磨板组件
-        meta: { title: '积分等级列表' }, //定义导航的标题
+        name: 'coreIntegralGradeList', //每个路由节点的name的名字不能相同
+        component: () => import('@/views/core/integral-grade/list'), //指向template模板组件
+        meta: { title: '积分等级列表' } //定义导航的标题
       },
       {
         path: 'create',
         name: 'coreIntegralGradeCreate',
         component: () => import('@/views/core/integral-grade/form'),
-        meta: { title: '新增积分等级' },
+        meta: { title: '新增积分等级' }
       },
       {
-        path: 'edit/:id', //：id 是一个占位符，表示这部分url会是任何一个id
+        path: 'edit/:id', // :id 是一个占位符，表示这部分url会是任何一个id
         name: 'coreIntegralGradeEdit',
         component: () => import('@/views/core/integral-grade/form'),
         meta: { title: '编辑积分等级' },
-        hidden: true,
-      },
-    ],
+        hidden: true
+      }
+    ]
   },
 
   {
@@ -102,9 +101,9 @@ export const constantRoutes = [
         path: 'list',
         name: 'coreUserInfoList',
         component: () => import('@/views/core/user-info/list'),
-        meta: { title: '会员列表' },
-      },
-    ],
+        meta: { title: '会员列表' }
+      }
+    ]
   },
 
   {
@@ -119,9 +118,9 @@ export const constantRoutes = [
         path: 'dict/list',
         name: '数据字典',
         component: () => import('@/views/core/dict/list'),
-        meta: { title: '数据字典' },
-      },
-    ],
+        meta: { title: '数据字典' }
+      }
+    ]
   },
 
   {
@@ -135,29 +134,31 @@ export const constantRoutes = [
         path: 'table',
         name: 'Table',
         component: () => import('@/views/table/index'),
-        meta: { title: 'Table', icon: 'table' },
+        meta: { title: '表格', icon: 'table' }
       },
       {
         path: 'tree',
         name: 'Tree',
         component: () => import('@/views/tree/index'),
-        meta: { title: 'Tree', icon: 'tree' },
-      },
-    ],
+        meta: { title: '树', icon: 'tree' }
+      }
+    ]
   },
 
   {
     path: '/form',
     component: Layout,
+    meta: { title: '一级标题', icon: 'el-icon-goods' },
+    alwaysShow: true,
     children: [
       {
         path: 'index',
         name: 'Form',
         component: () => import('@/views/form/index'),
-        meta: { title: 'Form', icon: 'form' },
-      },
-    ],
-  },
+        meta: { title: '表单', icon: 'form' }
+      }
+    ]
+  }
 ]
 
 /**
@@ -172,7 +173,7 @@ export const asyncRoutes = [
     name: 'Nested',
     meta: {
       title: 'Nested',
-      icon: 'nested',
+      icon: 'nested'
     },
     children: [
       {
@@ -185,7 +186,7 @@ export const asyncRoutes = [
             path: 'menu1-1',
             component: () => import('@/views/nested/menu1/menu1-1'),
             name: 'Menu1-1',
-            meta: { title: 'Menu1-1' },
+            meta: { title: 'Menu1-1' }
           },
           {
             path: 'menu1-2',
@@ -198,31 +199,31 @@ export const asyncRoutes = [
                 component: () =>
                   import('@/views/nested/menu1/menu1-2/menu1-2-1'),
                 name: 'Menu1-2-1',
-                meta: { title: 'Menu1-2-1' },
+                meta: { title: 'Menu1-2-1' }
               },
               {
                 path: 'menu1-2-2',
                 component: () =>
                   import('@/views/nested/menu1/menu1-2/menu1-2-2'),
                 name: 'Menu1-2-2',
-                meta: { title: 'Menu1-2-2' },
-              },
-            ],
+                meta: { title: 'Menu1-2-2' }
+              }
+            ]
           },
           {
             path: 'menu1-3',
             component: () => import('@/views/nested/menu1/menu1-3'),
             name: 'Menu1-3',
-            meta: { title: 'Menu1-3' },
-          },
-        ],
+            meta: { title: 'Menu1-3' }
+          }
+        ]
       },
       {
         path: 'menu2',
         component: () => import('@/views/nested/menu2/index'),
-        meta: { title: 'menu2' },
-      },
-    ],
+        meta: { title: 'menu2' }
+      }
+    ]
   },
 
   {
@@ -231,20 +232,20 @@ export const asyncRoutes = [
     children: [
       {
         path: 'https://panjiachen.github.io/vue-element-admin-site/#/',
-        meta: { title: 'External Link', icon: 'link' },
-      },
-    ],
+        meta: { title: 'External Link', icon: 'link' }
+      }
+    ]
   },
 
   // 404 page must be placed at the end !!!
-  { path: '*', redirect: '/404', hidden: true },
+  { path: '*', redirect: '/404', hidden: true }
 ]
 
 const createRouter = () =>
   new Router({
     // mode: 'history', // require service support
     scrollBehavior: () => ({ y: 0 }),
-    routes: constantRoutes,
+    routes: constantRoutes
   })
 
 const router = createRouter()
---
+++ srb-vue/src/views/core/user-info/list.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="app-container">user list</div>
+</template>
+
+<script>
+import userInfoApi from '@/api/core/user-info'
+
+export default {
+  data() {
+    return {
+      list: null, // 数据列表
+      total: 0, // 数据库中的总记录数
+      page: 1, // 默认页码
+      limit: 10, // 每页记录数
+      searchObj: {}, // 查询条件
+      loginRecordList: [], //会员登录日志
+      dialogTableVisible: false, //对话框是否显示
+    }
+  },
+
+  created() {
+    this.fetchData()
+  },
+
+  methods: {
+    fetchData() {
+      userInfoApi
+        .getPageList(this.page, this.limit, this.searchObj)
+        .then((response) => {
+          this.list = response.data.pageModel.records
+          this.total = response.data.pageModel.total
+        })
+    },
+  },
+}
+</script>
